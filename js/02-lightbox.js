ecf2cf4fcc382d0711339c2cbc702c981df76ef7
EmanVB <emanbonsato@yahoo.com>
2024-02-27 19:18:54 +0800

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,4 +3,29 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const gallery = document.querySelector
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((imgs) => {
+
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${imgs.original}">
+            <img
+                class="gallery__image"
+                src="${imgs.preview}"
+                alt="${imgs.description}"
+            />
+        </a>
+    </li>
+    `;
+    gallery.innerHTML += li;
+});
+
+gallery.addEventListener("click", (e) => {
+    console.log ("You clicked an image!");
+    if(e.target.tagName === "IMG"){
+        console.log (e.target);
+         let resultImg = new SimpleLightbox(".gallery a", {captionsData: alt});
+         resultImg.on ("show.simplelightbox");    
+        }
+});
